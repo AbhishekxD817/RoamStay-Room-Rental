f512245f025566273fcb1f40a901998880bba362
Abhishek Kumar <ab2004ak@gmail.com>
2024-11-10 20:04:06 +0530

    ready for deployment (Urls configure)

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ import cookieParser from 'cookie-parser';
 
 const app = express();
 const corsOptions = {
-    origin:"http://localhost:5173",
+    origin:"https://roamstay-room-rental.netlify.app/",
     optionsSuccessStatus: 200,
     credentials:true
 }
